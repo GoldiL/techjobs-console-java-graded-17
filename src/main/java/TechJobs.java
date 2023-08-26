643454b6c0d91232fda500aa1f96a991df6d2430
GoldiL <goldialbina@gmail.com>
2023-08-25 22:23:45 -0500

    PrintJobs updates

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //System.out.println("printJobs is not implemented yet");
+        if ( someJobs!= null && someJobs.size() > 0 ) {
+            for (HashMap<String, String> sJobs : someJobs) {
+                System.out.println("\n*****");
+
+                for (Map.Entry entry : sJobs.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+                System.out.println("*****");
+            }
+        }else{
+            System.out.println("No Results");
+        }
+
     }
 }
